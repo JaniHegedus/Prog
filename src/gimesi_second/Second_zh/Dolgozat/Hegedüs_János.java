032e6ff235ccdf6e37752fd2299ff2a9129d03c7
JaniHegedus <90450741+JaniHegedus@users.noreply.github.com>
2022-03-31 10:19:00 +0200

    New file

---
+++ src/gimesi_second/Second_zh/Dolgozat/Hegedüs_János.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package gimesi_second.Second_zh.Dolgozat;
+
+/**
+ *
+ * @author heged
+ */
+public class Hegedüs_János {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
