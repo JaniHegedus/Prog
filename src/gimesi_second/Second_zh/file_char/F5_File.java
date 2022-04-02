593f2d70808d13263173500a74986b8a9e171c0e
JaniHegedus <90450741+JaniHegedus@users.noreply.github.com>
2022-04-02 20:44:45 +0200

    Changes by andris

--- src/gimesi_second/Second_zh/file_char/F5_File.java
+++ src/gimesi_second/Second_zh/file_char/F5_File.java
@@ -1,8 +1,45 @@
 package gimesi_second.Second_zh.file_char;
 
+import java.io.*;
+import java.util.Scanner;
+
+import static java.lang.System.err;
+import static java.lang.System.out;
+
 public class F5_File
 {
+    public static void main(String[] args)
+    {
+        //meleg vagyok ok ooOOOOOOooooooOOOOooooOOOoOpooopOoo
+        //szeretem a fitymát, rák
+        //rákos FIDESZ legyek, fent a rákos hegyen
+        Scanner be = new Scanner(System.in);
+        try
+        {
+            PrintWriter ki = new PrintWriter("Dikaz.txt");
+            boolean csoro = true;
+            out.println("Sor szöveg:");
+            String dik = be.nextLine();
+            while(csoro)
+            {
+                if(!dik.equals(""))
+                {
+                    ki.println(dik);
+                    out.println("Sor szöveg:");
+                    dik = be.nextLine();
+                }
+                else
+                {
+                    csoro = false;
+                }
 
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            err.println("Anyád");
+        }
+    }
 }
 /*
 Készítsen programot, amely egy metódus segítségével sorokat olvas be a billentyűzetről
--- src/gimesi_second/Second_zh/file_char/F6_File.java
+++ src/gimesi_second/Second_zh/file_char/F6_File.java
@@ -1,7 +1,53 @@
 package gimesi_second.Second_zh.file_char;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static java.lang.System.*;
+import static java.lang.System.in;
+
 public class F6_File
 {
+    //RÁKOS SZAR EZ A KARAKTEROLVASÁS
+
+    public static void main(String[] args) {
+        String sor;
+        //char c;
+        //int sum;
+        int szumma = 0;
+        //ArrayList<String> idk = new ArrayList<>();
+        try
+        {
+            BufferedReader in = new BufferedReader(new FileReader("adatok.txt"));
+            while ((sor = in.readLine()) != null)
+            {
+                int i = 0;
+                while(i < sor.length())
+                {
+                    if(sor.charAt(i) == ' ')
+                    {
+                        szumma++;
+                    }
+                }
+            }
+            out.println(szumma);
+            in.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            err.println("Anyád");
+        }
+        catch (IOException r)
+        {
+            err.println("gec ez üres");
+        }
+        finally{
+            out.println("ilyen is van");
+        }
+    }
 
 }
 /*
